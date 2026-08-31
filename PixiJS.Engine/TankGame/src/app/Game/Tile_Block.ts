39c4c8c6a8502d1367f4b88948cc688f378473a9
xuke2026 <825126369@qq.com>
2026-08-31 19:37:11 +0800

    Update Tile_Block.ts

--- PixiJS.Engine/TankGame/src/app/Game/Tile_Block.ts
+++ PixiJS.Engine/TankGame/src/app/Game/Tile_Block.ts
@@ -1,6 +1,7 @@
-import { Bounds, Sprite } from "pixi.js";
+import { Bounds, Sprite, Ticker } from "pixi.js";
 import { TankLevel } from "./TankLevel";
 import { TileBase } from "./Tile";
+import { engine } from "../getEngine";
 
 export class Tile_Block extends TileBase
 {
@@ -21,18 +22,24 @@ export class Tile_Block extends TileBase
     public override Collider2DZone():Bounds
     {
         let bounds:Bounds = this.mSprite.getBounds();
+        console.log("Collider2DZone 111 bounds: " + bounds.toString());
         return bounds;
     }
-
+    
     public override showBounds() 
     {
         let bounds:Bounds = this.Collider2DZone();
         console.log("showBounds 222 bounds: " + bounds.toString());
-        this.boundsGraphics.tint = 0x00FF00;
+        this.boundsGraphics.clear();
         this.boundsGraphics.rect(bounds.x, bounds.y, bounds.width, bounds.height);
-        this.boundsGraphics.x = bounds.x;
-        this.boundsGraphics.y = bounds.y;
+        this.boundsGraphics.fill(0x00FF00, 0.5);
         // 5. 添加到舞台
-        this.addChild(this.boundsGraphics)
+        engine().stage.addChild(this.boundsGraphics)
     }
+    
+    public update(_time: Ticker) 
+    {
+        this.showBounds();
+    }
+
 }
