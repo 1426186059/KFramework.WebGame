ac3dc01fd371a10441ffc3fd03dfb79c98a2ce6c
xuke2026 <825126369@qq.com>
2026-09-02 12:34:33 +0800

    o

--- PixiJS.Engine/TankGame/src/main.ts
+++ PixiJS.Engine/TankGame/src/main.ts
@@ -3,11 +3,8 @@ import { LoadScreen } from "./app/screens/LoadScreen";
 import { MainScreen } from "./app/screens/main/MainScreen";
 import { userSettings } from "./app/utils/userSettings";
 import { CreationEngine } from "./engine/engine";
-
-/**
- * Importing these modules will automatically register there plugins with the engine.
- */
 import "@pixi/sound";
+import { initDevtools } from '@pixi/devtools';
 // import "@esotericsoftware/spine-pixi-v8";
 
 // Create a new creation engine instance
@@ -21,6 +18,8 @@ setEngine(engine);
     resizeOptions: { minWidth: 768, minHeight: 1024, letterbox: false },
   });
 
+ // window.__PIXI_DEVTOOLS__ = {engine};
+  initDevtools({ app:engine});
   // Initialize the user settings
   userSettings.init();
 
