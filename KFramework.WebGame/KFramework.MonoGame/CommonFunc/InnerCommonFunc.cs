a1982527bfa55863f4d21dfc0e52e0be0430a142
KK <1426186059@qq.com>
2026-09-15 21:31:53 +0800

    Create InnerCommonFunc.cs

---
+++ KFramework.WebGame/KFramework.MonoGame/CommonFunc/InnerCommonFunc.cs
@@ -0,0 +1,11 @@
+﻿namespace KFramework.MonoGame
+{
+    internal static class InnerCommonFunc
+    {
+        public static string DecodeUtf8(byte[] data)
+        {
+            int start = data.Length >= 3 && data[0] == 0xEF && data[1] == 0xBB && data[2] == 0xBF ? 3 : 0;
+            return System.Text.Encoding.UTF8.GetString(data, start, data.Length - start);
+        }
+    }
+}
