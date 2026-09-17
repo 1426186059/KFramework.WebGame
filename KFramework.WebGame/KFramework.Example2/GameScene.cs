415208620aa6ba7200cfbead059e2e91be973ef3
KK <1426186059@qq.com>
2026-09-18 03:45:10 +0800

    o

--- KFramework.WebGame/KFramework.Example2/GameScene.cs
+++ KFramework.WebGame/KFramework.Example2/GameScene.cs
@@ -60,13 +60,13 @@ public sealed class GameScene : KSceneBase
 
     public override void LoadContent()
     {
-        AssetBundle bundle = _content.GetBundle("Assets")
-            ?? throw new InvalidOperationException("内容包 content 尚未加载");
-        _res = ResCenter.Load(bundle, _gd);
-        _sounds = SoundCenter.Load(bundle);
+        AssetBundle Bundle = _content.GetBundle("main", false);
+
+        _res = ResCenter.Load(Bundle, _gd);
+        _sounds = SoundCenter.Load(Bundle);
         _hud = new Hud(_gd);
 
-        _level.Init(bundle, _sounds, _res);
+        _level.Init(Bundle, _sounds, _res);
         _level.Resize(_gd.Viewport.Width, _gd.Viewport.Height);   // 对应 PixiJS 初始化时算一次 fTileScaleCoef
 
         _uiRoot.Parent = SceneNodeRoot;
