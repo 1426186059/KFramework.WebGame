c030bf576e74c873ed25184e4a7ebda6b0163288
KK <1426186059@qq.com>
2026-09-18 02:53:08 +0800

    Update JsonTool.cs

--- KFramework.WebGame/KFramework.MonoGameExtend/Json/JsonTool.cs
+++ KFramework.WebGame/KFramework.MonoGameExtend/Json/JsonTool.cs
@@ -1,10 +1,7 @@
 ﻿using KTexturePacker.Parser;
-using System;
-using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Text.Json;
 using System.Text.Json.Serialization;
-using System.Text.Json.Serialization.Metadata;
 
 namespace KFramework.MonoGameExtend
 {
@@ -22,31 +19,5 @@ namespace KFramework.MonoGameExtend
     internal sealed partial class AppJsonContext : JsonSerializerContext
     {
         //这是Json 源产生器 
-    }
-
-    //专门为 AOT 设计的 Json 工具类
-    public static class JsonTool
-    {
-        public static string ToJson<T>(T value)
-        {
-            JsonTypeInfo<T> A = (JsonTypeInfo<T>)AppJsonContext.Default.GetTypeInfo(typeof(T));
-            return JsonSerializer.Serialize(value, A);
-        }
-
-        public static T? FromJson<T>(string json)
-        {
-            JsonTypeInfo<T> A = (JsonTypeInfo<T>)AppJsonContext.Default.GetTypeInfo(typeof(T));
-            return JsonSerializer.Deserialize(json, A);
-        }
-
-        public static string ToJson<T>(T value, JsonTypeInfo<T> A)
-        {
-            return JsonSerializer.Serialize(value, A);
-        }
-
-        public static T? FromJson<T>(string json, JsonTypeInfo<T> A)
-        {
-            return JsonSerializer.Deserialize(json, A);
-        }
     }
 }
